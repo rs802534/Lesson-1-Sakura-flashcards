2ec0919894f0f67c885e2112b6cb2c43d36606bd
rs802534 <115992279+rs802534@users.noreply.github.com>
2026-03-23 14:09:43 +0900

    add sakura app

---
+++ sakura-sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'sakura-v1';
+const STATIC = [
+  '/sakura-flashcards/',
+  '/sakura-flashcards/index.html',
+  '/sakura-flashcards/sakura-manifest.json'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.pathname.includes('sakura-words.json')) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      });
+    })
+  );
+});
